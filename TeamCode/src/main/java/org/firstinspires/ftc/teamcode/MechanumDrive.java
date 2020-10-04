72c15b0374048b3f918f57068a0d50bcb5280560
anika_l41je6u <thakur.2.anika@gmail.com>
2020-10-04 16:40:16 -0500

    mechanum drive 4 wheel anika

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MechanumDrive.java
@@ -50,8 +50,8 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
-@Disabled
+@TeleOp(name="MechanumDrive", group="Iterative Opmode")
+
 public class MechanumDrive extends OpMode
 {
     // Declare OpMode members.

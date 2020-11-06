3a53c18eea898c6e2e1d6bd3f2628760be2ccfcd
ProBots16446 <ProBots16446@users.noreply.github.com>
2020-11-06 11:05:54 -0600

    added left and right shooter

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MDrive.java
@@ -27,34 +27,19 @@ import org.firstinspires.ftc.teamcode.Shared.HardwareBot;
             telemetry.addData("Say", "Hell-O-Ween! Driver");
         }
 
-        //Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
         @Override
         public void init_loop() {
         }
 
-        //Code to run ONCE when the driver hits PLAY
         @Override
         public void start() {
         }
 
-        //Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
         @Override
         public void loop() {
 
-//Create and set values to used variables
-            double speed = 0, angle = 0, dchange = 0;
-            double retValues [];
-
-//Instance of cartesianToPolar method used for changing the cartesian values into polar values.
-            retValues = cartesianToPolar(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-
-//Set retValues array to shown values. retValues is used to return the variables used in multiple methods.
-            speed = retValues[0];
-            angle = retValues[1];
-            dchange = retValues[2];
-
-//Instance of polarToWheelSpeed method used for powering wheels
-            polarToWheelSpeed(speed, angle, dchange);
+            Drive();
+            Shoot();
 
 //Update the Telemetry
             telemetry.update();
@@ -64,83 +49,45 @@ import org.firstinspires.ftc.teamcode.Shared.HardwareBot;
         @Override
         public void stop() {
         }
-
-        public double[] cartesianToPolar(double y1, double x1, double turn)
-        {
-
-//Reset retValues to 0 for later use
-            double[] retValues = new double []{0.0,0.0,0.0};
-            double speed = 0.0, angle=0.0, dchange=0.0;
-
-//Change joypad values into useful polar values
-            speed = Math.sqrt((y1 * y1) + (x1 * x1));
-            angle = Math.atan2(-x1, y1);
-            dchange = turn;       //;/ 3.33;
-
-//Joypad input Telemetry
-            if (joyPosTele) {
-                telemetry.addData("X1: ",x1);
-                telemetry.addData("Y1: ",y1);
-                telemetry.addData("turn:",turn);
-            }
-
-//Polar values Telemetry
-            if (joyPolarCoordTele) {
-                telemetry.addData("Speed: ", speed);
-                telemetry.addData("Angle: ", angle);
-                telemetry.addData("Rotational Change", dchange);
-            }
-
-//Add polar values to retValues array
-            retValues[0] = speed;
-            retValues[1] = angle;
-            retValues[2] = dchange;
-            return retValues;
-        }
-
-        public void polarToWheelSpeed(double speed, double angle, double dchange){
-
-//Create Variables used only in this method
-            double pos1, pos2, pos3, pos4, maxValue;
-
-//Define unscaled voltage multipliers
-           // The front-right and back-left wheel should be set to sin(angle−1/4π) * magnitude.
-            // The front-left and back-right wheel should be set to sin(angle+1/4π) * magnitude.
-            pos1 = speed*Math.sin(angle+(Math.PI/4))+dchange; //Front Left
-            pos2 = speed*Math.sin(angle+(Math.PI/4))-dchange; //Front Right
-            pos3 = speed*Math.sin(angle+(Math.PI/4))+dchange; //Back Right
-            pos4 = speed*Math.sin(angle+(Math.PI/4))-dchange; // Back Left
-
-//VOLTAGE MULTIPLIER SCALER
-
-//Set maxValue to pos1 absolute
-            maxValue = Math.abs(pos1);
-
-//If pos2 absolute is greater than maxValue, then make maxValue equal to pos2 absolute
-            if(Math.abs(pos2) > maxValue){maxValue = Math.abs(pos2);}
-
-//If pos3 absolute is greater than maxValue, then make maxValue equal to pos3 absolute
-            if(Math.abs(pos3) > maxValue){maxValue = Math.abs(pos3);}
-
-//If pos4 absolute is greater than maxValue, then make maxValue equal to pos4 absolute
-            if(Math.abs(pos4) > maxValue){maxValue = Math.abs(pos4);}
-
-//Check if need to scale -- if not set to 1 to nullify scale
-            if (maxValue <= 1){ maxValue = 1;}
-
-//Power motors with scaled voltage multipliers
-            robot.FrontLeft.setPower(pos1/maxValue);
-            robot.FrontRight.setPower(pos2/maxValue);
-            robot.BackLeft.setPower(pos3/maxValue);
-            robot.BackRight.setPower(pos4/maxValue);
-
-//Scaled Voltage Multiplier Telemetry
-            if (wheelScalersTele) {
-                telemetry.addData("Wheel 1 W/ Scale: ",pos1/maxValue);
-                telemetry.addData("Wheel 2 W/ Scale: ",pos2/maxValue);
-                telemetry.addData("Wheel 3 W/ Scale: ",pos3/maxValue);
-                telemetry.addData("Wheel 4 W/ Scale: ",pos4/maxValue);
-            }
-        }
+    public double limit(double value) {
+        return Math.max(-1, Math.min(value, 1));
     }
+        public void Drive()
+        {
+            double multipliar=1.25;
+            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
+            double rightX = gamepad1.right_stick_x;
+            final double v1 = (r * Math.cos(robotAngle) - rightX)*multipliar;
+            final double v2 = (r * Math.sin(robotAngle) + rightX)*multipliar;
+            final double v3 = (r * Math.sin(robotAngle) - rightX)*multipliar;
+            final double v4 = (r * Math.cos(robotAngle) + rightX)*multipliar;
+
+            robot.FrontLeft.setPower(limit(v1));
+            robot.FrontRight.setPower(limit(v2));
+            robot.BackLeft.setPower(limit(v3));
+            robot.BackRight.setPower(limit(v4));
+
+            telemetry.addData("Speed: ", v1);
+            telemetry.update();
+
+        }
+    public void Shoot()
+    {
+
+        double leftTriggerSpeed =gamepad1.left_trigger;
+        double rightTriggerSpeed = -1*gamepad1.right_trigger;
+
+        robot.ShooterLeft.setPower(limit(leftTriggerSpeed));
+        robot.ShooterLeft.setPower(limit(rightTriggerSpeed));
+
+        telemetry.addData("Speed: ", leftTriggerSpeed);
+        telemetry.addData("Speed: ", rightTriggerSpeed);
+        telemetry.update();
+
+    }
+
+}
+
+
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shared/HardwareBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shared/HardwareBot.java
@@ -22,6 +22,8 @@ public class HardwareBot {
     public DcMotor FrontRight = null;
     public DcMotor BackLeft = null;
     public DcMotor BackRight = null;
+    public DcMotor ShooterLeft = null;
+    public DcMotor ShooterRight = null;
 
     /* Local OpMode members. */
     HardwareMap hwMapRobot = null;
@@ -41,25 +43,30 @@ public class HardwareBot {
         FrontRight = hwMapRobot.dcMotor.get("frontRight");
         BackLeft = hwMapRobot.dcMotor.get("backLeft");
         BackRight = hwMapRobot.dcMotor.get("backRight");
-
+        ShooterLeft = hwMapRobot.dcMotor.get("shooterLeft");
+        ShooterRight = hwMapRobot.dcMotor.get("shooterRight");
 //Define Motor Directions
         FrontLeft.setDirection(DcMotor.Direction.FORWARD);
         FrontRight.setDirection(DcMotor.Direction.REVERSE);
         BackLeft.setDirection(DcMotor.Direction.FORWARD);
-        BackRight.setDirection(DcMotor.Direction.REVERSE);
-
+        BackRight.setDirection(DcMotor.Direction.FORWARD);
+        ShooterLeft.setDirection(DcMotor.Direction.FORWARD);
+        ShooterRight.setDirection(DcMotor.Direction.FORWARD);
 // Set all motors to zero power
         FrontLeft.setPower(0);
         FrontRight.setPower(0);
         BackLeft.setPower(0);
         BackRight.setPower(0);
-
+        ShooterLeft.setPower(0);
+        ShooterRight.setPower(0);
 // Set all motors to run with encoders.
 // May want to use RUN_USING_ENCODER if encoders are installed.
         FrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS );
         FrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS );
         BackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS );
         BackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS );
+        ShooterLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS );
+        ShooterRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS );
 
     }
 }
